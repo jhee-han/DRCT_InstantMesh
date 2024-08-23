cdf71e7d5ce5822edef342f1ad9ef231edb6ccde
juhee <juhee7148@gmail.com>
2024-08-23 17:42:45 +0000

    Initial commit

---
+++ sr_run.py
@@ -0,0 +1,224 @@
+import os
+import argparse
+import numpy as np
+import torch
+from PIL import Image
+from torchvision.transforms import v2
+from pytorch_lightning import seed_everything
+from omegaconf import OmegaConf
+from einops import rearrange
+from huggingface_hub import hf_hub_download
+from diffusers import DiffusionPipeline, EulerAncestralDiscreteScheduler
+
+from src.utils.train_util import instantiate_from_config
+from src.utils.camera_util import (
+    FOV_to_intrinsics, 
+    get_zero123plus_input_cameras,
+    get_circular_camera_poses,
+)
+from src.utils.mesh_util import save_obj, save_obj_with_mtl
+from src.utils.infer_util import save_video
+from torchvision.transforms import ToTensor
+
+def get_render_cameras(batch_size=1, M=120, radius=4.0, elevation=40.0, is_flexicubes=False):
+    """
+    Get the rendering camera parameters.
+    """
+    c2ws = get_circular_camera_poses(M=M, radius=radius, elevation=elevation)
+    if is_flexicubes:
+        cameras = torch.linalg.inv(c2ws)
+        cameras = cameras.unsqueeze(0).repeat(batch_size, 1, 1, 1)
+    else:
+        extrinsics = c2ws.flatten(-2)
+        intrinsics = FOV_to_intrinsics(30.0).unsqueeze(0).repeat(M, 1, 1).float().flatten(-2)
+        cameras = torch.cat([extrinsics, intrinsics], dim=-1)
+        cameras = cameras.unsqueeze(0).repeat(batch_size, 1, 1)
+    return cameras
+
+def render_frames(model, planes, render_cameras, render_size=512, chunk_size=1, is_flexicubes=False):
+    """
+    Render frames from triplanes.
+    """
+    frames = []
+    for i in range(0, render_cameras.shape[1], chunk_size):
+        if is_flexicubes:
+            frame = model.forward_geometry(
+                planes,
+                render_cameras[:, i:i+chunk_size],
+                render_size=render_size,
+            )['img']
+        else:
+            frame = model.forward_synthesizer(
+                planes,
+                render_cameras[:, i:i+chunk_size],
+                render_size=render_size,
+            )['images_rgb']
+        frames.append(frame)
+    
+    frames = torch.cat(frames, dim=1)[0]    # we suppose batch size is always 1
+    return frames
+
+###############################################################################
+# Arguments.
+###############################################################################
+
+parser = argparse.ArgumentParser()
+parser.add_argument('config', type=str, help='Path to config file.')
+parser.add_argument('--output_path', type=str, default='outputs/', help='Output directory.')
+parser.add_argument('--diffusion_steps', type=int, default=75, help='Denoising Sampling steps.')
+parser.add_argument('--seed', type=int, default=42, help='Random seed for sampling.')
+parser.add_argument('--scale', type=float, default=1.0, help='Scale of generated object.')
+parser.add_argument('--distance', type=float, default=4.5, help='Render distance.')
+parser.add_argument('--view', type=int, default=6, choices=[4, 6], help='Number of input views.')
+parser.add_argument('--no_rembg', action='store_true', help='Do not remove input background.')
+parser.add_argument('--export_texmap', action='store_true', help='Export a mesh with texture map.')
+parser.add_argument('--save_video', action='store_true', help='Save a circular-view video.')
+parser.add_argument('--drct', action='store_true', help='Use DRCT images for reconstruction.')
+parser.add_argument('--ipg', action='store_true', help='Use IPG images for reconstruction.')
+args = parser.parse_args()
+seed_everything(args.seed)
+
+###############################################################################
+# Stage 0: Configuration.
+###############################################################################
+
+config = OmegaConf.load(args.config)
+config_name = os.path.basename(args.config).replace('.yaml', '')
+model_config = config.model_config
+infer_config = config.infer_config
+
+IS_FLEXICUBES = True if config_name.startswith('instant-mesh') else False
+
+device = torch.device('cuda')
+
+# load reconstruction model
+print('Loading reconstruction model ...')
+model = instantiate_from_config(model_config)
+if os.path.exists(infer_config.model_path):
+    model_ckpt_path = infer_config.model_path
+else:
+    model_ckpt_path = hf_hub_download(repo_id="TencentARC/InstantMesh", filename=f"{config_name.replace('-', '_')}.ckpt", repo_type="model")
+state_dict = torch.load(model_ckpt_path, map_location='cpu')['state_dict']
+state_dict = {k[14:]: v for k, v in state_dict.items() if k.startswith('lrm_generator.')}
+model.load_state_dict(state_dict, strict=True)
+
+model = model.to(device)
+if IS_FLEXICUBES:
+    model.init_flexicubes_geometry(device, fovy=30.0)
+model = model.eval()
+
+# make output directories
+image_path = os.path.join(args.output_path, config_name, 'images')
+mesh_path = os.path.join(args.output_path, config_name, 'meshes')
+video_path = os.path.join(args.output_path, config_name, 'videos')
+os.makedirs(image_path, exist_ok=True)
+os.makedirs(mesh_path, exist_ok=True)
+os.makedirs(video_path, exist_ok=True)
+
+###############################################################################
+# Stage 1: Multiview generation.
+###############################################################################
+
+# Automatically collect image paths based on arguments
+script_dir = os.path.dirname(os.path.realpath(__file__))
+
+if args.drct:
+    base_path = os.path.join(script_dir, "DRCT", "results")
+elif args.ipg:
+    base_path = os.path.join(script_dir, "IPG", "results", "train_IPG_SR_DF2K_x4_500000", "visualization", "InferenceDataset")
+else:
+    raise ValueError("Either --drct or --ipg must be specified.")
+
+image_paths = sorted([os.path.join(base_path, f) for f in os.listdir(base_path) if f.endswith(".png")])[:6]
+
+# Load and preprocess images
+images = []
+transform = ToTensor()
+
+for path in image_paths:
+    img = Image.open(path)
+    img_tensor = transform(img)
+    images.append(img_tensor)
+
+# Convert to tensor format
+images = torch.stack(images)
+images = images.unsqueeze(0).to(device)  # Add batch dimension and move to GPU
+images = v2.functional.resize(images, 320, interpolation=3, antialias=True).clamp(0, 1)
+
+# SR한 이미지들(1280)을 320,640,1280 으로 resolution을 변화시켰을 때 이미지의 해상도 변화를 알 수 있다.
+# Save the preprocessed images for viewing
+# for idx, img_tensor in enumerate(images.squeeze(0)):
+#     img = img_tensor.permute(1, 2, 0).cpu().numpy()
+#     img = (img * 255).astype(np.uint8)
+#     img_pil = Image.fromarray(img)
+#     img_pil.save(os.path.join(image_path, f'preprocessed_view_{idx + 1}.png'))
+
+
+
+###############################################################################
+# Stage 2: Reconstruction.
+###############################################################################
+
+input_cameras = get_zero123plus_input_cameras(batch_size=1, radius=4.0*args.scale).to(device)
+chunk_size = 20 if IS_FLEXICUBES else 1
+
+for idx, sample in enumerate(images):
+    image_name = os.path.basename(image_paths[idx]).replace('.png', '')
+    name = f"sr_{image_name}"
+    print(f'[{idx+1}/{len(images)}] Creating {name} ...')
+
+    with torch.no_grad():
+        # get triplane
+        planes = model.forward_planes(images, input_cameras)
+
+        # get mesh
+        mesh_path_idx = os.path.join(mesh_path, f'{name}.obj')
+
+        mesh_out = model.extract_mesh(
+            planes,
+            use_texture_map=args.export_texmap,
+            **infer_config,
+        )
+        if args.export_texmap:
+            vertices, faces, uvs, mesh_tex_idx, tex_map = mesh_out
+            save_obj_with_mtl(
+                vertices.data.cpu().numpy(),
+                uvs.data.cpu().numpy(),
+                faces.data.cpu().numpy(),
+                mesh_tex_idx.data.cpu().numpy(),
+                tex_map.permute(1, 2, 0).data.cpu().numpy(),
+                mesh_path_idx,
+            )
+        else:
+            vertices, faces, vertex_colors = mesh_out
+            save_obj(vertices, faces, vertex_colors, mesh_path_idx)
+        print(f"Mesh saved to {mesh_path_idx}")
+
+        # get video
+        if args.save_video:
+            video_path_idx = os.path.join(video_path, f'{name}.mp4')
+            render_size = infer_config.render_resolution
+            render_cameras = get_render_cameras(
+                batch_size=1, 
+                M=120, 
+                radius=args.distance, 
+                elevation=20.0,
+                is_flexicubes=IS_FLEXICUBES,
+            ).to(device)
+            
+            frames = render_frames(
+                model, 
+                planes, 
+                render_cameras=render_cameras, 
+                render_size=render_size, 
+                chunk_size=chunk_size, 
+                is_flexicubes=IS_FLEXICUBES,
+            )
+
+            save_video(
+                frames,
+                video_path_idx,
+                fps=30,
+            )
+           
+        print(f"Video saved to {video_path}")
